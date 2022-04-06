3035e4db26224380726c537a66d67a44a54cf0cb
tuoying96 <tuoying96@gmail.com>
2022-04-05 23:30:01 -0700

    [Setup] init service_campus
    Done:
    - created service_campus;
    - creates ServiceCampusApplication; Spring can run

---
+++ finalproject/nustudy_parent/service/service_campus/src/main/java/com/group2/campus/ServiceCampusApplication.java
@@ -0,0 +1,17 @@
+package com.group2.campus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: Ying Tuo
+ * Created Time: $[Date]
+ * Description:
+ */
+@SpringBootApplication
+public class ServiceCampusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCampusApplication.class, args);
+    }
+
+}
