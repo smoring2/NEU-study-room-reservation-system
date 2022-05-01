75fe94888e33df04508fda28bb55f27338110554
smoring2 <smoring1995@gmail.com>
2022-05-01 13:40:29 -0700

    cleaned rabbit_util

--- finalproject/nustudy_parent/common/rabbit_util/src/main/java/com/group2/common/rabbit/config/MQConfig.java
+++ finalproject/nustudy_parent/common/rabbit_util/src/main/java/com/group2/common/rabbit/config/MQConfig.java
@@ -4,6 +4,9 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Rabbitmq configuration
+ */
 @Configuration
 public class MQConfig {
 
--- finalproject/nustudy_parent/common/rabbit_util/src/main/java/com/group2/common/rabbit/service/RabbitService.java
+++ finalproject/nustudy_parent/common/rabbit_util/src/main/java/com/group2/common/rabbit/service/RabbitService.java
@@ -4,6 +4,9 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * RabbitMQ message queue
+ */
 @Service
 public class RabbitService {
 
@@ -11,10 +14,11 @@ public class RabbitService {
     private RabbitTemplate rabbitTemplate;
 
     /**
-     *  发送消息
-     * @param exchange 交换机
-     * @param routingKey 路由键
-     * @param message 消息
+     * send message
+     *
+     * @param exchange
+     * @param routingKey
+     * @param message
      */
     public boolean sendMessage(String exchange, String routingKey, Object message) {
         rabbitTemplate.convertAndSend(exchange, routingKey, message);
