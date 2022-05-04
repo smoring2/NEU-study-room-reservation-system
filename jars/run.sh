c2276ec035e6539cb2f218762bda751dde6d328d
smoring2 <smoring1995@gmail.com>
2022-05-03 18:11:31 -0700

    fixed can not generate jar file issue

---
+++ jars/run.sh
@@ -0,0 +1,6 @@
+java -jar service_campus-0.0.1-SNAPSHOT.jar &
+java -jar campus_manage-0.0.1-SNAPSHOT.jar &
+java -jar service_gateway-0.0.1-SNAPSHOT.jar &
+java -jar service_cmn-0.0.1-SNAPSHOT.jar &
+java -jar service_order-0.0.1-SNAPSHOT.jar &
+java -jar service_user-0.0.1-SNAPSHOT.jar 
